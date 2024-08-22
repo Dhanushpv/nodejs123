821ce73c37ead78915b0c46aa3c7555cb10be6c7
dhanushpvenkitesh <dhanushpvenkitesh@gmail.com>
2024-08-22 09:43:56 +0530

    first

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const port = 3000;
+const url =require('url')
+const dhanu = require('fs')
+const server = http.createServer((req,res)=>{
+    const req_url = req.url;
+    console.log('req-url :',req_url)
+
+    const parsed_url = url.parse(req_url);
+    console.log('parsed-url',parsed_url);
+
+    if(parsed_url.pathname === '/'){
+        res.writeHead(200,{'Content-Type' : 'text/html'})
+        res.end(dhanu.readFileSync('./index.html'))
+    }
+    else if (parsed_url.pathname === '/anitta'){
+        res.writeHead(200,{'Content-Type':'text/json'});
+        res.end(dhanu.readFileSync('datas.json'));
+    }
+    
+    else if(parsed_url.pathname === '/style.css'){
+        res.writeHead(200,{'Content-type': 'text/css'})
+        res.end(dhanu.readFileSync('./style.css'))
+    }
+})
+server.listen(port,()=>{
+    console.log(`server running at http://localhost:${port}`)
+})
